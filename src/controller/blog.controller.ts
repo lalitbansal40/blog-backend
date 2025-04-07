f6a8b942cd9839e3b50b2f0a064e1955767877d0
lalitbansal12345 <165453425+lalitbansal40@users.noreply.github.com>
2025-04-07 22:55:51 +0530

    changes

--- src/controller/blog.controller.ts
+++ src/controller/blog.controller.ts
@@ -4,8 +4,12 @@ import { BlogModel } from "../model/blog.model";
 
 export const getBlogList = async (req: Request, res: Response) => {
     try {
-        const blogs = await BlogListModel.find();
-        res.status(200).json(blogs);
+        const page: number = parseInt(req.query.page as string) || 1; // Current page, default to 1
+        const limit = parseInt(req.query.limit as string) || 10; // Items per page, default to 10
+        const skip = (page - 1) * limit;
+        const blogs = await BlogListModel.find().skip(skip).limit(limit);
+        const total: number = await BlogListModel.countDocuments();
+        res.status(200).json({ blogs: blogs, totalPages: Math.ceil(total / limit) });
     } catch (error) {
         res.status(500).json({ message: 'Error fetching blogs', error });
     }
